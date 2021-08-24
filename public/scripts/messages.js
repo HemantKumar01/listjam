549bcf94b2f4f03e01355125c50294d5874aeb78
HemantKumar <ytbhemant@gmail.com>
2021-08-24 13:43:57 +0530

    Revert "version 2"
    This reverts commit f2b2811c55132ea48a817da518a7d6eef4a0471a.

--- public/scripts/messages.js
+++ public/scripts/messages.js
@@ -1,49 +1,20 @@
-const workspace = document.querySelector(".workspace");
+var workspace = document.querySelector(".workspace");
 /*
 * an array of all message objects;
 ? message object = 
 ?    message {
-?        id: userEmailId + "m" + messageNumber
+?        id:"m" + messageNumber
 ?        title: "",
 ?        time: Date().now(),
 ?        status:"done|todo",
-?        priority:"will_do_later|normal|important",
-*       //below items are used only for sent/recieved messages. 
-*       //also message object does not include id, it will be added while reading from db
-?        owner: "me"|friendObject:{name, pic, email},
-?        sentBy: FriendOwner {name, email, pic}
+?        priority:"will_do_later|normal|important"
 ?    }
 */
 var messages = []
 
 
-//* html element to add to workspace after rendering friend's messages.
-const sendInputText = `
-<div id="sendInput" class = "flexbox">
-    <select id = "priority"
-    onchange = "changeSelectColorBasedOnPriority()"
-    required>
-        <option value = "will_do_later"> Less Important </option>
-        <option value = "normal" selected> Normal </option>
-        <option value = "important"> Important </option> 
-    </select>
-    <input type = "text"
-     max = "100"
-     placeholder = "send..." id="sendItem" />
-    <button class="sendButton" onclick = "sendMessage()">
-        <i class = "fas fa-paper-plane"> </i>
-    </button>
-</div>
-`
-
-/*
-*function to return HTML for a list items
-?   @PARAM:
-?       1.) message: a message Object as defined above;
-?       2.) isMyList: is the message created by me(+ recieved) or showing in sent
-?           [sent list items' checkbox should be disabled]
-*/
-function getListItem(message, isMyList = true, sentBySomeone = false) {
+//function to return HTML for a list items
+function getListItem(message) {
     var {
         id,
         title,
@@ -52,132 +23,56 @@ function getListItem(message, isMyList = true, sentBySomeone = false) {
         priority = "normal"
     } = message
     const dateTime = new Date(time);
-    if (message.sentBy) {
-        return `<div class="message ${priority} sent">
-        ${isMyList ?    //? if my list, then show checkbox, else if friends list then hide checkbox
-            `<div class="close" data-id="${id}" onclick='removeItem(this.getAttribute("data-id"))'>x</div>
+
+    return `<div class="message ${priority}">
+
                 <input class= "list"
                 type = "checkbox"
-                ${message.id ? `onclick = "changeStatus('${message.id}')"` : " "}  
-                ${status == "done" ? "checked" : " "}>`
-            :
-                ""
-            }
-            <div class="title">${title}</div>
-            <img class="sentAvatar" src=${message.sentBy.pic}>
-            <div class="time">
-                <p>${dateTime.toLocaleDateString()} </p>
-                <p>${dateTime.toLocaleTimeString()}</p>
-            </div>                
-        </img>`
+                onclick = "changeStatus('${message.id}')" 
+                ${ status == "done" ? "checked" : " "}>
 
-    } else {
-        return `<div class="message ${priority}">
-                ${isMyList ?    //? if my list, then show checkbox, else if friends list then hide checkbox
-                `<div class="close" data-id="${id}" onclick='removeItem(this.getAttribute("data-id"))'>x</div>
-                    
-                    <input class= "list"
-                    type = "checkbox"
-                    ${message.id ? `onclick = "changeStatus('${message.id}')"` : " "}  
-                    ${status == "done" ? "checked" : " "}>`
-                :
-                ""
-                }
                 <div class="title">${title}</div>
 
                 <div class="time">
                     <p>${dateTime.toLocaleDateString()} </p>
                     <p>${dateTime.toLocaleTimeString()}</p>
-                </div>                
+                </div>
+
+                <div class="close" data-id="${id}" onclick='removeItem(this.getAttribute("data-id"))'>x</div>
+                
             </div>`
-    }
 }
 
+function showMessages() {
+    var messages = localStorage.messages;
 
-/* 
- ? PARAM: 
- ? 1.) person: the person whose messages to show(default to 'me'(my List), it should be email of friend)
- */
-function showMessages(person = "me") {
-    workspace.innerHTML = "";
-
-    if (person == "me") {
-        if (navigator.onLine) {
-            if (isUserSignedIn())
-                checkForNewMessages(getEmail());
-        }
-
-        window.friendActive = null;
-
-        document.querySelector(".infoPane").style.display = "none"
-
-        var messages = localStorage.messages;
-        if (!messages) {
-            showEmpty();
-            return;
-        }
-        messages = JSON.parse(messages);
-    } else {
-        window.friendActive = person; //? to know whom to send from same ui;
-        setInfoPaneContent(person)
-        document.querySelector(".newFriend").style.display = "none";
-
-        var messages = localStorage.friendMessages;
-        if (!messages) {
-            showEmpty("try sending something to your new list-jam friend", "friend");
-            return;
-        }
-
-        //* defining messages to show it later down below in this function
-        messages = JSON.parse(messages)[person]; //? show person's message (person = email of friend)
-
+    if (!messages) {
+        showEmpty();
+        return;
     }
 
+    messages = JSON.parse(messages);
+
     var query = window.query;
-    var isMyList = (person == "me");
+    workspace.innerHTML = "";
 
     var empty = true
     messages.forEach((message) => {
         if (query == "all" || query == message.status.toLowerCase()) {
             empty = false
-            workspace.innerHTML += getListItem(message, isMyList, (message.sentBy != null));
+            workspace.innerHTML += getListItem(message);
         }
     })
-    if (empty) { //? even if message object is present, and nothing is rendered by above method(the object is empty) then showEmpty
+    if (empty) {
         showEmpty();
-        return;
-    }
-    if (person != "me") {
-        //* add input and button to send something to the friend.
-        workspace.innerHTML += sendInputText;
-        const sendInputElement = document.querySelector("#sendInput");
-        let sendInputTextInput = document.querySelector("#sendInput #sendItem"); //the actual input inside sendInput element
-        onEnterPress(
-            sendInputTextInput,
-            document.querySelector("#sendInput .sendButton")
-        ) //? while entering a text in ${sendInput} input field, if enter is pressed, then send list item
-
-        //*positioning the sendInput box (input box to send messages to friends) at the bottom
-        sendInputElement.style.bottom = "auto";
-        sendInputElement.style.top = workspace.scrollHeight - sendInputElement.clientHeight + "px";
-
-        //*scrolling to bottom so that user always see recent messages first
-        //* and can send messages easily(user can scroll upwards(opposite) to see previous messages)
-        sendInputElement.scrollIntoView();
-
     }
 }
-//* person = me|friend
-function showEmpty(text, person = "me") {
+
+function showEmpty() {
     workspace.style.alignContent = "center";
     workspace.style.justifyContent = "center";
-    workspace.style.flexDirection = "row"
 
     workspace.innerHTML = "";
-
-    if (!text) {
-        text = "woohoo! you have completed all of your list"
-    }
 
     workspace.innerHTML +=
         `<div class = "flexbox empty"
@@ -188,15 +83,10 @@ function showEmpty(text, person = "me") {
             height = "auto"
             alt = "nothing in here. list empty" />
 
-            <h3> Nothing in here.</h3>
-            <p> ${text} </p>
+            <h3> Nothing in here. </h3>
+            <p> woohoo! you have completed all of your list </p>
         </div>
         `
-
-    if (person != "me") {
-        //* add input and button to send something to the friend.
-        workspace.innerHTML += sendInputText
-    }
 }
 
 function changeStatus(id) {
--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,69 +1,41 @@
 //just v-3 of cache will be present in users device as others are deleted
-const cacheName = "listJamCache-v5";
+const cacheName = "listJamCache-v4";
 
-const cacheFirstCache = [
-    //? these files below will be first searched from cache instead of internet
+const contentToCache = [
+    '/index.html',
+    '/style.css',
+
     '/images/sticky-notes-outline.png',
-    '/images/Google_Logo.png',
     '/images/og_image.png',
     '/images/icon.png',
     '/favicon.ico',
 
     '/scripts/components.js',
-    '/scripts/messages.js', ,
-    '/scripts/send.js',
-    '/scripts/auth.js',
+    '/scripts/firebaseSettings.js',
+    '/scripts/messages.js',
+    '/scripts/serviceWorker.js'
 ];
 
-const internetFirstCache = [
-    //? these files below will be first searched from cache instead of internet
-    '/index.html',
-    '/style.css',
-
-    "/scripts/update.js",
-    '/scripts/serviceWorker.js',
-    '/scripts/firebaseSettings.js',
-    '/scripts/firestore.js'
-]
 
 //* load content(cache first, if not found search over internet)
 self.addEventListener('fetch', (e) => {
     e.respondWith((async () => {
-        //?if in internetFirstCache first load from internet, in case of unavailabilty load from cache.
-        if (internetFirstCache.includes(e.request.url)) {
-            return fetch(e.request).then((response) => {
-                caches.open(cacheName).then((cache) => {
-                    try {
-                        cache.put(e.request, response.clone());
-                    } catch (err) {
-                        console.debug(`[service worker]: cannot cache response of ${e}
-                            error: ${err}
-                        `)
-                    }
-                });
-                return response;
-            }).catch(() => {
-                return caches.match(e.request);
-            })
-        }
-
-        //? first load from cache, and if not found then look over internet and cache it;
         const cachedContent = await caches.match(e.request);
-        console.debug(`[service worker]: Fetching resource: ${e.request.url}`);
-        if (cachedContent) { //? if cache found
+        console.log(`[service worker]: Fetching resource: ${e.request.url}`);
+        if (cachedContent) { //if cache found
             return cachedContent;
         }
-        const response = await fetch(e.request);
 
-        //? if cache not found then fetch it and add to cache
+        //if cache not found then fetch it and add to cache
+        const response = await fetch(e.request);
         try {
             if (e.request.method !== 'GET') {
                 const cache = await caches.open(cacheName);
-                console.debug(`[service worker]: Caching new resource: ${e.request.url}`);
+                console.log(`[service worker]: Caching new resource: ${e.request.url}`);
                 cache.put(e.request, response.clone());
             }
         } catch (err) {
-            console.debug(`[service worker]: cannot cache response of ${e}
+            console.log(`[service worker]: cannot cache response of ${e}
                 response: ${response};
                 error: ${err}
             `)
@@ -76,13 +48,13 @@ self.addEventListener('fetch', (e) => {
 
 //*init the cache
 self.addEventListener('install', (e) => {
-    console.debug("[service worker]: install event fired.")
+    console.log("[service worker]: install event fired.")
 
     e.waitUntil((async () => {
         const cache = await caches.open(cacheName);
 
-        console.debug("[service worker]: Caching all necessary content, to make the app work offline.")
-        await cache.addAll(cacheFirstCache.concat(internetFirstCache));
+        console.log("[service worker]: Caching all necessary content, to make the app work offline.")
+        await cache.addAll(contentToCache);
     })())
 
 })
